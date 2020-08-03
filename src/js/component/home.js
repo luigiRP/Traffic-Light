9ef898ed886cf1300bb4e15c946922f4addd42cd
luigiRP <luigirosales84@gmail.com>
2020-08-03 11:00:34 +0000

    first

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [color, setColor] = useState("");
+
+	switch (color) {
+		case "red":
+			// code block
+			return (
+				<div>
+					<div className="trafficStick" />
+					<div className="trafficWhole d-flex flex-column justify-content-around">
+						<div
+							className="light my-1 red active"
+							onClick={() => setColor("red")}
+						/>
+						<div
+							className="light my-1 yellow"
+							onClick={() => setColor("yellow")}
+						/>
+						<div
+							className="light my-1 green"
+							onClick={() => setColor("green")}
+						/>
+					</div>
+				</div>
+			);
+			break;
+		case "yellow":
+			// code block
+			return (
+				<div>
+					<div className="trafficStick" />
+					<div className="trafficWhole d-flex flex-column justify-content-around">
+						<div
+							className="light my-1 red "
+							onClick={() => setColor("red")}
+						/>
+						<div
+							className="light my-1 yellow active"
+							onClick={() => setColor("yellow")}
+						/>
+						<div
+							className="light my-1 green"
+							onClick={() => setColor("green")}
+						/>
+					</div>
+				</div>
+			);
+			break;
+		case "green":
+			return (
+				<div>
+					<div className="trafficStick" />
+					<div className="trafficWhole d-flex flex-column justify-content-around">
+						<div
+							className="light my-1 red "
+							onClick={() => setColor("red")}
+						/>
+						<div
+							className="light my-1 yellow "
+							onClick={() => setColor("yellow")}
+						/>
+						<div
+							className="light my-1 green active"
+							onClick={() => setColor("green")}
+						/>
+					</div>
+				</div>
+			);
+			break;
+
+		default:
+			return (
+				<div>
+					<div className="trafficStick" />
+					<div className="trafficWhole d-flex flex-column justify-content-around">
+						<div
+							className="light my-1 red "
+							onClick={() => setColor("red")}
+						/>
+						<div
+							className="light my-1 yellow "
+							onClick={() => setColor("yellow")}
+						/>
+						<div
+							className="light my-1 green "
+							onClick={() => setColor("green")}
+						/>
+					</div>
+				</div>
+			);
+		// code block
+	}
 }
